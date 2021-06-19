f2d7c2d774a1c94a3d0d8f0903e98eada12b8e2e
sammysamp <sammysamp@gmail.com>
2021-06-18 20:52:39 -0700

    0x01:Task65.0:example_of_for_using_puchar_and_ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: respuesta
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar (n);
+	putchar('\n');
+	return (0);
+
+}
+
