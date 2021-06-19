c6edb85a60280e5520df2a59d1b77625910bfa2b
sammysamp <sammysamp@gmail.com>
2021-06-18 20:56:58 -0700

    0x01:Task7.0:example_of_for_decreasing_by_1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: respuesta
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar (n);
+	putchar ('\n');
+	return (0);
+}
+
