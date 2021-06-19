209d2b9004636c4e85c2cbfdaa12557c2b8cbad5
sammysamp <sammysamp@gmail.com>
2021-06-18 20:46:59 -0700

    0x01:Task5.0:example_of_for_3

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: respuesta
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+
+}
+
