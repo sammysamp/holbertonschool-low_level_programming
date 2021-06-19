45cec630c3740f14c82d1d09f4452df1acd0668a
sammysamp <sammysamp@gmail.com>
2021-06-18 20:10:41 -0700

    0x01:Task2.0:example_of_putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Return: respuesta
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+		putchar (n);
+	putchar ('\n');
+	return (0);
+}
+
