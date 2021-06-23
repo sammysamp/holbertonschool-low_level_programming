cc95fb6da39a33233fab84ebdfc273641bd57745
sammysamp <sammysamp@gmail.com>
2021-06-23 12:57:59 -0700

    0x03:Task2.0 Debugging logic

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * largest_number - function
+ * @a: first int
+ * @b: second int
+ * @c: third int
+ *
+ * Return: largest
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if ((a > b && a > c) || (a == b && a > c) || (a == c && a > b))
+	{
+		largest = a;
+	}
+	else if ((b > a && b > c) || (b == a && b > c) || (b == c && b > a))
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
