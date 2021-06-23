c6845485933baf0f58466a80c2461f269d086c2f
sammysamp <sammysamp@gmail.com>
2021-06-23 09:13:23 -0700

    0x02:Task10.0 Sum 2 integers and return the result directly with return

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * add - writes the character c to stdout
+ * @a: first int
+ * @b: second int
+ *
+ * Return: On success 1.
+ *
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
+
