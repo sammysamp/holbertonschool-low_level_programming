67827cf286357f65edbe1746ca21508239c68db5
sammysamp <sammysamp@gmail.com>
2021-06-23 19:45:15 -0700

    0x02:Task6.0 function print absolute value exercise

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - return the absolute value
+ * @c: integer value to convert
+ *
+ * Return: 1 on success
+ *
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}
