6f64c3d25ca8c5c2ea02a8fa77ff46166a3b1d38
sammysamp <sammysamp@gmail.com>
2021-06-23 19:58:53 -0700

    0x02:Task7.0 function print last digit exercise

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_last_digit - return the last digit of a given number
+ * @c: integer to check
+ *
+ * Return: 1 on success
+ *
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
