43a4dd4c28862239c335fec692501cefe035e8ab
sammysamp <sammysamp@gmail.com>
2021-06-24 19:10:14 -0700

    0x04:Task2.0 mul customized function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplicate 2 numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: retur directly the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
