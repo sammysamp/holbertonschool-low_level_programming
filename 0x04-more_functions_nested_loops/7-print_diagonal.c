2f668bccf5821410c4b7ce5e24118a7315d19a68
sammysamp <sammysamp@gmail.com>
2021-06-24 21:03:54 -0700

    0x04:Task7.0 print diagonal function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: number of repetitions
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
