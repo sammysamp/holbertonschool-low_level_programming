43bc82abc952534091a0b466dc089035c8cb14e0
sammysamp <sammysamp@gmail.com>
2021-06-24 20:47:20 -0700

    0x04:Task6.0 print line function

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_line - print line
+ * @n: number of repetitions
+ * Return: nothing
+ */
+
+void print_line(int n);
+{
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
