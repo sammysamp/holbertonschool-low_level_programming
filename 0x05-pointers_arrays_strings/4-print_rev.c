174eca84089f1c6265e2f25f76bd9e1199ef44f0
sammysamp <sammysamp@gmail.com>
2021-06-28 20:30:03 -0700

    0x05:Task4 return string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse
+ * @s: pointer of the string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s = s - len;
+	for (i = len; i >= 0; i--)
+	{
+		printf("%c", *(s + i));
+	}
+	printf("\n");
+}
