c23956df0649eb45a1b6439403f1eacf948b068d
sammysamp <sammysamp@gmail.com>
2021-07-02 16:35:19 -0700

    0x06: Task3 comparing 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (s1 == s2)
+	while (*s1 == *s2)
 	{
-		if (s1 == '\0')
+		if (*s1 == '\0')
 			return (0);
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2)
+	return (*s1 - *s2);
 }
