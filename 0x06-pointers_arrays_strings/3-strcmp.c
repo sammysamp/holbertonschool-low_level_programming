2467763718153e81fd253695786f6fb6ec1a7266
sammysamp <sammysamp@gmail.com>
2021-07-02 16:27:56 -0700

    0x06: Task3 comparing 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _strcmp - compare 2 strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: the diference in int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (s1 == s2)
+	{
+		if (s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2)
+}
