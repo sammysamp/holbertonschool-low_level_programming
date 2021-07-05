72fef24960b0c39928449b8e52e21f7ed29055ff
sammysamp <sammysamp@gmail.com>
2021-07-05 15:40:17 -0700

    0x07 Task 2: finding char into string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strchr - find char on a string
+ * @s: string where to look for
+ * @c: character to look forl
+ *
+ * Return: pointer of the first occurrence
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return ('\0');
+}
+
+
+
