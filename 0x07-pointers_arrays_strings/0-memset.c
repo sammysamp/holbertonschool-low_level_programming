115daffd1ebe3f110952015e0cbec01f2031f7e7
sammysamp <sammysamp@gmail.com>
2021-07-05 15:17:20 -0700

    0x07 Task 0: fill memory on existing string

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+/**
+ * _memset - fill values on the array
+ * @s: original memory representation
+ * @b: character to print on memory
+ * @n: the size of the memory to fill
+ *
+ * Return: New print of memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
+
+
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This directory has projects about exercises with pointer, }
+arrays and strings on C on Holberton
