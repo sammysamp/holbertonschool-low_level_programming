549d87874b3db1d76653664c48a6e6191cc1775a
sammysamp <sammysamp@gmail.com>
2021-07-06 08:46:04 -0700

    0x07: Task 6 print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - print chessboard
+ * @a: array
+ *
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int chess [8][8];
+	int row=0,column=0;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
