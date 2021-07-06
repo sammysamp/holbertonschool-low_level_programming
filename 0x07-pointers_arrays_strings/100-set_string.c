d7b72b3d3c523a12d7d2c11a808ae2d960de98ac
sammysamp <sammysamp@gmail.com>
2021-07-06 10:48:39 -0700

    0x07: Task 8 pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: a double pointer
+ * @to: a pointer
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
