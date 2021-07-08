ad0bc68b4588fddf5da121c7c57cf81a73869883
sammysamp <sammysamp@gmail.com>
2021-07-07 20:17:02 -0700

    0x08 Task 3 calculating factorial with recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * factorial - calculate factorial of given number
+ * @n: number to calculate factorial
+ *
+ * Return: factorial of given number
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
