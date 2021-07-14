51558f2eb173f422200680f12cd79fc73d758381
sammysamp <sammysamp@gmail.com>
2021-07-13 21:26:09 -0700

    0x0B Task 1: create a copy of array in a function using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strdup - copy a string on other portion of memory
+ * @str: string to copy
+ *
+ * Return: pointer to array
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	int i;
+	int j;
+
+	if (str == NULL)
+		return (NULL);
+	j = 0;
+	while (str[j])
+		j++;
+	i = 0;
+	ar = malloc(sizeof(*ar) * j - 1);
+	if (ar == NULL)
+		return (NULL);
+	while (i < j)
+	{
+		ar[i] = str[i];
+		i++;
+	}
+	return (ar);
+}
