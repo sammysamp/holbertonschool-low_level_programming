3d3756970610346bab42df34431a5839c34cb2d6
sammysamp <sammysamp@gmail.com>
2021-07-15 09:26:34 -0700

    0x0B Task 3: using 2D array with malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * alloc_grid - create a 2 dimensional array
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: new 2 dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **mygrid;
+	int i;
+	int j;
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+	mygrid = malloc(sizeof(int *) * height);
+	if (mygrid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		mygrid[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+			mygrid[i][j] = 0;
+	}
+	return (mygrid);
+}
