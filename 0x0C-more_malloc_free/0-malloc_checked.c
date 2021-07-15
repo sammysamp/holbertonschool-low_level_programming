e49ae80c92a28c6ad3713ee6988b9080c6b40f9d
sammysamp <sammysamp@gmail.com>
2021-07-15 15:09:30 -0700

    0x0C Task 0 Allocate memory function and return a pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - check the code for Holberton School students.
+ * @b:
+ *
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This project have more exercises to practice dinamic allocation memory with malloc and free
