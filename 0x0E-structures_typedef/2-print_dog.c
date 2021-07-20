8d7831d7d67f7ff6e4823ef2d83068cb99408c95
sammysamp <sammysamp@gmail.com>
2021-07-19 21:24:48 -0700

    0x0E Task 2: printing members of given struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - print struct members
+ * @d: struct pointer
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
+
