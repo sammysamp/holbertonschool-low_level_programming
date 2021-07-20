114d66dad632586b3cc653acd0d25c7d7e98a890
sammysamp <sammysamp@gmail.com>
2021-07-19 20:57:15 -0700

    0x0E Task 1: function to initialize a struct using pointer

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - init dog structure.
+ * @d: struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
