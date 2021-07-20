556c407ea397509646bac822499867f08b1d022a
sammysamp <sammysamp@gmail.com>
2021-07-20 11:59:06 -0700

    0x0E Task 4: creating new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - create new dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: new dog pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		exit(98);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+
+	return (d);
+}
