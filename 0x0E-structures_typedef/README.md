959187aff17419bf81d1bc4b579e62bdc0b4c8f8
sammysamp <sammysamp@gmail.com>
2021-07-19 20:10:45 -0700

    0x0E Task 0 creating the first structure on h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Here are exercises of structures and typedef in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog
+#define dog
+/**
+ * struct dog - first structure of dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
