a9d75de14b931814c57d59229da1b00cfb7cdb2b
sammysamp <sammysamp@gmail.com>
2021-07-21 15:03:41 -0700

    0x0F Task 0:Print name using function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: function pointer
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
