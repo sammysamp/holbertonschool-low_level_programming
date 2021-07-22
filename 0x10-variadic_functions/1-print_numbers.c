daf0d2ee78b12bd7ca8370218d1ed37e507004c6
sammysamp <sammysamp@gmail.com>
2021-07-22 15:09:37 -0700

    0x10 task 1: Print elements using variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print elements
+ * @separator: string separator
+ * @n: number of elements (integer)
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d %s", va_arg(ap, unsigned int), separator);
+	printf("%d", va_arg(ap, unsigned int));
+	va_end(ap);
+	printf("\n");
+}
