c96ccb724a5fe0b9c6e2bf35429933da6264ec19
sammysamp <sammysamp@gmail.com>
2021-07-23 09:24:38 -0700

    0x0F Task1: execute given function using function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL)
 		return;
+	if (action == NULL)
+		return;
 	mname = malloc(sizeof(action));
 	if (mname == NULL)
 		return;
