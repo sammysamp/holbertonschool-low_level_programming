ee1fe23d55c882129126603d5148bf33f6736845
sammysamp <sammysamp@gmail.com>
2021-07-23 07:51:12 -0700

    0x0F Task0: print name using function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTERS.H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
