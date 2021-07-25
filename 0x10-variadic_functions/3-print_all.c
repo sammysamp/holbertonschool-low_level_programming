6ceb86d0f824d2572a5c0b2ba8dfa592b3958d4d
sammysamp <sammysamp@gmail.com>
2021-07-24 21:48:21 -0700

    0x10 task 3: print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct fmt
+{
+	char in;
+	void (*fn)(va_list);
+} fmt;
+
+void fnChar(va_list al)
+{
+	printf("%c", va_arg(al, int));
+}
+void fnInt(va_list al)
+{
+	printf("%d", va_arg(al, int));
+}
+void fnStr(va_list al)
+{
+	printf("%s", va_arg(al, char*));
+}
+void fnFloat(va_list al)
+{
+	printf("%f", va_arg(al, double));
+}
+
+void print_all(const char * const format, ...)
+{
+	fmt fmts[] = {
+		{'c', fnChar},
+		{'i', fnInt},
+		{'f', fnFloat},
+		{'s', fnStr},
+		{'\0', NULL}
+	};
+
+	int iformat = 0;
+	int ifmt;
+	va_list al;
+	char *separator = "";
+
+	va_start(al, format);
+
+	while (format && format[iformat] != '\0')
+	{
+		ifmt = 0;
+		while (fmts[ifmt].in != '\0')
+		{
+			if (fmts[ifmt].in == format[iformat])
+			{
+				printf("%s", separator);
+				fmts[ifmt].fn(al);
+				separator = ", ";
+				break;
+			}
+			ifmt++;
+		}
+		iformat++;
+	}
+	va_end(al);
+	printf("\n");
+}
