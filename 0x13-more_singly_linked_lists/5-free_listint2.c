7c218c17931ce45b398c84a2fcc3540fb3c5f027
sammysamp <sammysamp@gmail.com>
2021-08-04 20:44:05 -0700

    freeing memory and set pointer to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_listint2 - free memory of a linked list
+ * @head: origin pointer linked list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
