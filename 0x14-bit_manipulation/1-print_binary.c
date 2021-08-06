f6a49b02c36ec6eee439203df4b60ae6cb50d16d
sammysamp <sammysamp@gmail.com>
2021-08-05 20:24:06 -0700

    convert decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_binary - convert unsigned int to bin
+ * @n: number tu convert
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int mask = 32768;
+	int flag = 0;
+	
+	if (n == 0)
+		printf("0");
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag == 1)
+				printf("0");
+		}
+		else
+		{
+			flag = 1;
+			printf("1");
+		}
+		mask = mask >> 1;
+	}
+}
