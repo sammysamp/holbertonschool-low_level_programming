963b065e8086f6e54365602439ac975508ddd241
sammysamp <sammysamp@gmail.com>
2021-08-06 18:29:16 -0700

    get nth node given a index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get the nth node
+ * @head: origin pointer linked list
+ * @index: index
+ * Return: n node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	unsigned int i;
+
+	if (head == NULL)
+		return (0);
+	current = head;
+	for (i = 0; i < index; i++)
+	{
+		current = current->next;
+	}
+	return (current);
+}
