f7155d7fe47774507a9085191e750c5c296c0ded
sammysamp <sammysamp@gmail.com>
2021-08-10 11:49:28 -0700

    getting a bit given an index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * get_bit - return bit diven an index
+ * @n: n
+ * @index: index
+ * Return: value of bit of given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bin_max = sizeof(n) * 8;
+
+	if (index > bin_max)
+		return (-1);
+	return ((n >> index) & 1);
+}
