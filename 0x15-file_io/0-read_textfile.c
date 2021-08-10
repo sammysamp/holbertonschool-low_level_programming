d827b76f0dfd02d344980bc9cd96b3bffc221bec
sammysamp <sammysamp@gmail.com>
2021-08-09 19:37:03 -0700

    reading text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
-	if (filename == NULL)
+	if (filename == NULL || letter < 0 || letter == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
