ea9f2c85376bcf63b1b56347a4ca2b16f5c01ea7
sammysamp <sammysamp@gmail.com>
2021-08-09 20:09:08 -0700

    creating and writing a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+/**
+ * create_file - create file
+ * @filename: file name
+ * @text_content: number of chars to read
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	char *buf;
+	ssize_t wd;
+
+	buf = malloc(sizeof(char) * strlen(text_content));
+	if (buf == NULL)
+		return (0);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	wd = write(fd, text_content, strlen(text_content));
+	if (wd == -1)
+		return (-1);
+	close(fd);
+	free(buf);
+	return (1);
+}
