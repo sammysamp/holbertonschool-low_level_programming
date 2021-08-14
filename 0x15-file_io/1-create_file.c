af1ebf4d21d90e397263523df39a846f2da2ea34
sammysamp <sammysamp@gmail.com>
2021-08-14 11:51:36 -0700

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (*text_content == '\0')
 		return (-1);
+	if (filename == NULL)
+		return (-1);
 	buf = malloc(sizeof(char) * strlen(text_content));
 	if (buf == NULL)
 		return (0);
-	if (filename == NULL)
-		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
