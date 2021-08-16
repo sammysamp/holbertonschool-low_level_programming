d75211d19f9da4bdcde0351da5421f2b881b92e3
sammysamp <sammysamp@gmail.com>
2021-08-16 13:49:03 -0700

    basic operation with function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,12 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (98);
 	}
-
+	if (!(*argv[2] == '+' || *argv[2] == '-' || *argv[2] == '*' ||
+				*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		return (99);
+	}
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
 	return (result);
