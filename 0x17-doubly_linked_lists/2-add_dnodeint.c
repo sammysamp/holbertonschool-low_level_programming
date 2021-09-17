bf24a62562f9f92ef527cc80f9b968f1f9e01ff4
sammysamp <sammysamp@gmail.com>
2021-09-17 12:06:59 -0700

    addinf a node at the beginning of dlist

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,36 +1,21 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a linked list
- * @head: a list of elements with a pointer to the first element
- * @n: integer to create the new node
+ * add_dnodeint - adds a new node at the beginning of a list
+ * @head: the head of a list
+ * @n: the piece of data of the new node
  *
  * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
-	{
-		free(new_node);
+	if (new == NULL)
 		return (NULL);
-	}
-	new_node->n = n;
-	if (new_node->n == '\0')
-	{
-		free(new_node);
-		return (NULL);
-	}
-	if (head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-	}
-	else
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	return (new_node);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (new);
 }
